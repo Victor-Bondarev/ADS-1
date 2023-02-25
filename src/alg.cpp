2581c4777f4b417fbf8ce6aa53f1c529fb416807
Victor Bondarev <125218061+Victor-Bondarev@users.noreply.github.com>
2023-02-25 21:47:50 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,6 +4,7 @@
 
 
 bool checkPrime(uint64_t value) {
+    // вставьте код функции
     bool checkPrime = true;
     if (value == 0 || value == 1) {
         checkPrime = false;
@@ -20,6 +21,7 @@ bool checkPrime(uint64_t value) {
 }
 
 uint64_t nPrime(uint64_t n) {
+    // вставьте код функции
     int i = 2;
     while (n > 0) {
         if (checkPrime(i)) {
@@ -32,6 +34,7 @@ uint64_t nPrime(uint64_t n) {
 }
 
 uint64_t nextPrime(uint64_t value) {
+    // вставьте код функции
     if (value <= 1) {
         return 2;
     }
@@ -49,6 +52,7 @@ uint64_t nextPrime(uint64_t value) {
 }
 
 uint64_t sumPrime(uint64_t hbound) {
+    // вставьте код функции
     int sum = 0;
     for (int i = 0; i < hbound; i++) {
         sum += i;
